9da4b654226a2bf3ae5186d30cac512d12e772f8
Sameer <rajbharasameer246810@gmail.com>
2026-04-12 12:09:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,87 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+import torch
+import re 
+from fastapi.templating import Jinja2Templates # UI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+
+# Initialize our fastapi app
+
+app = FastAPI(title="Text Summarizer APP", description="Text Summarization using T5", version="1.0")
+
+# load Model
+
+model = T5ForConditionalGeneration.from_pretrained("./saved_summary_model")
+tokenizer = T5Tokenizer.from_pretrained("./saved_summary_model")
+
+# Fixed Divece Error
+
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+elif torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+model.to(device) 
+
+# Templatiing
+
+templates = Jinja2Templates(directory=".")
+
+# Input Scheme For dialogue => Stirng
+
+class DialogueInput(BaseModel):
+    dialogue: str
+
+# Fuction for Removing html tags and lines, spaces
+
+def clean_data(text):
+    if not isinstance(text, str):
+        return ""
+    text = re.sub(r"\r\n", " ", text)
+    text = re.sub(r"\s+", " ", text)
+    text = re.sub(r"<.*?>", " ", text)
+    text = text.strip().lower()
+    return text
+
+# Clean Data to give Model
+
+def summarize_dialogue(dialogue: str) -> str:
+    dialogue = clean_data(dialogue)
+
+    inputs = tokenizer(
+        dialogue,
+        padding="max_length",
+        max_length=512,
+        truncation=True,
+        return_tensors="pt"
+    ).to(device)
+
+    model.to(device)
+    targets = model.generate(
+        input_ids=inputs["input_ids"],
+        attention_mask=inputs["attention_mask"],
+        max_length=150,
+        num_beams=4,
+        early_stopping=True
+    )
+
+    summary = tokenizer.decode(targets[0], skip_special_tokens=True)
+    return summary   
+
+
+# API endpoints
+
+# API endpoints
+@app.post("/summarize/")
+async def summarize(dialogue_input: DialogueInput):
+    summary = summarize_dialogue(dialogue_input.dialogue)
+    return {"summary": summary}
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
